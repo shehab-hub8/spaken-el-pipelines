47c639f684656fbe6298b60e29595b622a9d344d
shehab-hub8 <sheriffarouk443_sd2024@techedu.bsu.edu.eg>
2025-12-09 12:22:44 +0200

    Update sqlpoolscrept.sql

--- datawarehouse/sqlpoolscrept.sql
+++ datawarehouse/sqlpoolscrept.sql
@@ -1,103 +1,91 @@
-
-حازم السعدى
-11:37 م
--- 1) (Optional) Create DB master key if not exists
-CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Alsaady@#01141208078';
+-- 1. إنشاء قاعدة البيانات (لأنها غير موجودة)
+CREATE DATABASE hospital;
 GO
 
--- 2) Create database scoped credential (managed identity example)
--- For managed identity: use IDENTITY = 'Managed Identity' and DO NOT supply SECRET.
+-- 2. الدخول داخل قاعدة البيانات الجديدة
+USE hospital;
+GO
+
+-- 3. إنشاء مفتاح التشفير (مهم للأمان)
+-- (إذا أعطى خطأ أنه موجود مسبقاً، تجاهله)
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'Str0ngP@ssw0rd!';
+GO
+
+-- 4. إنشاء الاعتماد (Credential) للوصول للستوريدج
+-- نستخدم Managed Identity الخاصة بالسينابس
 CREATE DATABASE SCOPED CREDENTIAL storage_credential
 WITH IDENTITY = 'Managed Identity';
 GO
 
--- If you prefer SAS or storage account key, use:
--- CREATE DATABASE SCOPED CREDENTIAL storage_credential
--- WITH IDENTITY = 'SHARED ACCESS SIGNATURE', SECRET = 'sv=...&sig=...';
--- GO
-
--- 3) Create external data source pointing to ADLS Gen2 (ABFSS) using the credential
+-- 5. تعريف مصدر البيانات (Data Source)
+-- يشير إلى حاوية Gold
 CREATE EXTERNAL DATA SOURCE gold_data_source
 WITH (
-    TYPE = HADOOP,
-    LOCATION = 'abfss://gold@hospitalstorge.dfs.core.windows.net/',
+    LOCATION = 'abfss://gold@hospitalstorge17.dfs.core.windows.net/',
     CREDENTIAL = storage_credential
 );
 GO
 
--- 4) Define Parquet format
-CREATE EXTERNAL FILE FORMAT ParquetFileFormat
+-- 6. إنشاء View جدول المرضى (Patient)
+CREATE OR ALTER VIEW dbo.dim_patient_view
+AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'gold_dim_patient/',
+    DATA_SOURCE = 'gold_data_source',
+    FORMAT = 'DELTA'
+) 
 WITH (
-    FORMAT_TYPE = PARQUET
-);
+    patient_id VARCHAR(50),
+    gender VARCHAR(10),
+    age INT,
+    effective_from DATETIME2,
+    surrogate_key BIGINT,
+    effective_to DATETIME2,
+    is_current BIT
+) AS [r];
 GO
 
--- 5) External tables (examples)
-CREATE EXTERNAL TABLE dbo.hospital_performance_kpi (
-    hospital_name VARCHAR(50),
-    city VARCHAR(50),
-    total_patients BIGINT,
-    avg_stay_days FLOAT,         -- بدل double
-    critical_cases BIGINT,
-    icu_admissions BIGINT,
-    last_updated DATETIME2,
-    processing_date DATETIME2
+-- 7. إنشاء View جدول الأقسام (Department)
+CREATE OR ALTER VIEW dbo.dim_department_view
+AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'gold_dim_department/',
+    DATA_SOURCE = 'gold_data_source',
+    FORMAT = 'DELTA'
 )
 WITH (
-    LOCATION = 'hospital_performance_kpi/',
-    DATA_SOURCE = gold_data_source,
-    FILE_FORMAT = ParquetFileFormat
-);
+    surrogate_key BIGINT,
+    department NVARCHAR(200),
+    hospital_id INT
+) AS [r];
 GO
 
-SELECT * 
-FROM dbo.hospital_performance_kpi;
-
-DROP EXTERNAL TABLE dbo.hospital_performance_kpi;
-
-CREATE EXTERNAL TABLE dbo.disease_geography_trends (
-    governorate VARCHAR(50),
-    diagnosis VARCHAR(200),
-    gender VARCHAR(20),
-    case_count BIGINT,
-    avg_treatment_days FLOAT,   -- بدل double
-    latest_case VARCHAR(200),
-    last_updated TIME,          -- لو هو TIME فعلاً في الباركيت
-    processing_date TIME        -- لو هو TIME فعلاً في الباركيت
+-- 8. إنشاء View جدول الحقائق (Fact)
+CREATE OR ALTER VIEW dbo.fact_patient_flow_view
+AS
+SELECT *
+FROM OPENROWSET(
+    BULK 'gold_fact/',
+    DATA_SOURCE = 'gold_data_source',
+    FORMAT = 'DELTA'
 )
 WITH (
-    LOCATION = 'disease_geography_trends/',
-    DATA_SOURCE = gold_data_source,
-    FILE_FORMAT = ParquetFileFormat
-);
+    fact_id BIGINT,
+    patient_sk BIGINT,
+    department_sk BIGINT,
+    admission_time DATETIME2,
+    discharge_time DATETIME2,
+    admission_date DATE,
+    length_of_stay_hours FLOAT,
+    is_currently_admitted BIT,
+    bed_id INT,
+    event_ingestion_time DATETIME2
+) AS [r];
 GO
 
-
-
-
-SELECT * 
-FROM dbo.disease_geography_trends;
-
-
-
-
-CREATE EXTERNAL TABLE dbo.doctor_performance_kpi (
-    doctor_full_name varchar(50),
-    specialization varchar(50),
-    hospital_name varchar(50),
-    patients_treated BIGINT,
-    critical_cases_handled BIGINT,
-    avg_treatment_days FLOAT,
-    last_updated DATETIME,
-    processing_date DATETIME
-)
-WITH (
-    LOCATION = 'doctor_performance_kpi/',
-    DATA_SOURCE = gold_data_source,
-    FILE_FORMAT = ParquetFileFormat
-);
-GO
-
--- 6) Quick test (serverless: you can also use OPENROWSET for ad hoc tests)
-SELECT TOP 20 * FROM dbo.doctor_performance_kpi;
-‪eqj-jfym-ybc‬‏
+-- 9. اختبار نهائي
+SELECT TOP 10 * FROM dbo.fact_patient_flow_view;
+SELECT TOP 10 * FROM dbo.dim_patient_view;
+SELECT TOP 10 * FROM dbo.dim_department_view;
